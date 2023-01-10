0d882c65a0fc3369ad1723250e00276245de54a3
ansifi <104885099+ansifi@users.noreply.github.com>
2023-01-10 11:50:01 +0530

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppComponent } from './app.component';
 import { StackchartComponent } from './stackchart.component';
 import { LinechartComponent } from './linechart.component';
 import { PiechartComponent } from './piechart.component';
+import { Line1Component } from './line1.component';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, StackchartComponent, LinechartComponent, PiechartComponent],
+  declarations: [AppComponent, StackchartComponent, LinechartComponent, PiechartComponent, Line1Component],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/line1.component.ts
@@ -0,0 +1,121 @@
+import { Component, Input, OnInit, ElementRef } from '@angular/core';
+import * as echarts from 'echarts';
+
+import $ from 'jquery';
+
+@Component({
+  selector: 'line1',
+  template: `<div class="mGraph-wrapper">
+  <div class="mGraph" id="line1"></div>
+</div>`,
+  styles: [
+    `
+  .mGraph-wrapper{
+    width: 1000px;
+    height: 239px;
+    background: #fff;
+    position: relative;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .mGraph-wrapper .mGraph{
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }`,
+  ],
+})
+export class Line1Component implements OnInit {
+ 
+ // prettier-ignore
+const data = [["2000-06-05",116],["2000-06-06",129],["2000-06-07",135],["2000-06-08",86],["2000-06-09",73],["2000-06-10",85],["2000-06-11",73],["2000-06-12",68],["2000-06-13",92],["2000-06-14",130],["2000-06-15",245],["2000-06-16",139],["2000-06-17",115],["2000-06-18",111],["2000-06-19",309],["2000-06-20",206],["2000-06-21",137],["2000-06-22",128],["2000-06-23",85],["2000-06-24",94],["2000-06-25",71],["2000-06-26",106],["2000-06-27",84],["2000-06-28",93],["2000-06-29",85],["2000-06-30",73],["2000-07-01",83],["2000-07-02",125],["2000-07-03",107],["2000-07-04",82],["2000-07-05",44],["2000-07-06",72],["2000-07-07",106],["2000-07-08",107],["2000-07-09",66],["2000-07-10",91],["2000-07-11",92],["2000-07-12",113],["2000-07-13",107],["2000-07-14",131],["2000-07-15",111],["2000-07-16",64],["2000-07-17",69],["2000-07-18",88],["2000-07-19",77],["2000-07-20",83],["2000-07-21",111],["2000-07-22",57],["2000-07-23",55],["2000-07-24",60]];
+
+
+  constructor(private elm: ElementRef) {}
+  ngOnInit() {
+    
+    const dateList = this.data.map(function (item) {
+      return item[0];
+    });
+    const valueList = this.data.map(function (item) {
+      return item[1];
+    });
+
+    let lineChart = echarts.init(
+      $(this.elm.nativeElement).find('#line1')[0]
+    );
+    
+    lineChart.setOption({
+        // Make gradient line here
+        visualMap: [
+          {
+            show: false,
+            type: 'continuous',
+            seriesIndex: 0,
+            min: 0,
+            max: 400
+          },
+          {
+            show: false,
+            type: 'continuous',
+            seriesIndex: 1,
+            dimension: 0,
+            min: 0,
+            max: dateList.length - 1
+          }
+        ],
+        title: [
+          {
+            left: 'center',
+            text: 'Gradient along the y axis'
+          },
+          {
+            top: '55%',
+            left: 'center',
+            text: 'Gradient along the x axis'
+          }
+        ],
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: [
+          {
+            data: dateList
+          },
+          {
+            data: dateList,
+            gridIndex: 1
+          }
+        ],
+        yAxis: [
+          {},
+          {
+            gridIndex: 1
+          }
+        ],
+        grid: [
+          {
+            bottom: '60%'
+          },
+          {
+            top: '60%'
+          }
+        ],
+        series: [
+          {
+            type: 'line',
+            showSymbol: false,
+            data: valueList
+          },
+          {
+            type: 'line',
+            showSymbol: false,
+            data: valueList,
+            xAxisIndex: 1,
+            yAxisIndex: 1
+          }
+        ]
+    });
+  }
+}
